74fc4cd8746ccdaee5c1822dacc5b8f0d9a5e6e1
Rodrigo Bravo <bravo.rodrigo@gmail.com>
2017-06-29 20:03:55 -0300

    First TODO

--- app/src/main/java/br/com/laborumtech/wincomanda/LoginActivity.java
+++ app/src/main/java/br/com/laborumtech/wincomanda/LoginActivity.java
@@ -1,7 +1,10 @@
 package br.com.laborumtech.wincomanda;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +13,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    //TODO : Auto open keyboard in this activity
+    //EditText yourEditText= (EditText) findViewById(R.id.campoLogin);
+    //InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    //imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
 }
