776422d86c3197a27c4a5bc211931028aa9612f6
Rodrigo Bravo <bravo.rodrigo@gmail.com>
2017-06-29 23:40:17 -0300

    Login com teclado automatico

--- app/src/main/java/br/com/laborumtech/wincomanda/LoginActivity.java
+++ app/src/main/java/br/com/laborumtech/wincomanda/LoginActivity.java
@@ -3,6 +3,9 @@ package br.com.laborumtech.wincomanda;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -13,8 +16,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-    //TODO : Auto open keyboard in this activity
-    //EditText yourEditText= (EditText) findViewById(R.id.campoLogin);
-    //InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-    //imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+   // EditText campoLogin = (EditText) findViewById(R.id.campoLogin);
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_NUMPAD_ENTER)
+        {
+            Log.d("Keycode", "Got KeyCode 9");
+            return super.onKeyDown(keyCode, event);
+        }
+
+        return true;
+    }
 }
--- app/src/main/java/br/com/laborumtech/wincomanda/MainActivity.java
+++ app/src/main/java/br/com/laborumtech/wincomanda/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
